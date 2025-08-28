88c914528ea799ad9338b969705f9e4038cbebce
Kwame Oduro <odurokwameee@gmail.com>
2025-08-28 17:07:06 +0000

    utils: add test data manager for handling test data storage and retrieval

---
+++ src/test/java/com/swaglabs/utils/TestDataManager.java
@@ -0,0 +1,64 @@
+package com.swaglabs.utils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+
+public class TestDataManager {
+
+    private static Properties properties;
+    private static TestDataManager instance;
+
+    private TestDataManager() {
+        loadProperties();
+    }
+
+    public static TestDataManager getInstance() {
+        if (instance == null) {
+            instance = new TestDataManager();
+        }
+        return instance;
+    }
+
+    private void loadProperties() {
+        properties = new Properties();
+        try (InputStream inputStream = getClass().getClassLoader()
+                .getResourceAsStream("test-data.properties")) {
+            if (inputStream != null) {
+                properties.load(inputStream);
+            } else {
+                throw new RuntimeException("test-data.properties file not found in resources");
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to load test-data.properties", e);
+        }
+    }
+
+    public String getProperty(String key) {
+        return properties.getProperty(key);
+    }
+
+
+    // Product Methods
+    public String getBackpackName() {
+        return getProperty("product.backpack");
+    }
+
+    public String getBikeLightName() {
+        return getProperty("product.bikelight");
+    }
+
+    // Product Price Methods
+    public String getBackpackPrice() {
+        return getProperty("price.backpack");
+    }
+
+    public String getBikeLightPrice() {
+        return getProperty("price.bikelight");
+    }
+
+    public String getCartHeader() {
+        return getProperty("header.cart");
+    }
+}
