203de175e0cbe986f93455895f2a39d398452225
Kwame Oduro <odurokwameee@gmail.com>
2025-08-28 17:08:19 +0000

    utils: add test data utilities for test data helper methods

---
+++ src/test/java/com/swaglabs/utils/TestDataUtils.java
@@ -0,0 +1,9 @@
+package com.swaglabs.utils;
+
+public class TestDataUtils {
+
+    public static final String CURRENT_USER = "current_user";
+    public static final String CURRENT_PRODUCT = "current_product";
+    public static final String CHECKOUT_INFO = "checkout_info";
+
+}
