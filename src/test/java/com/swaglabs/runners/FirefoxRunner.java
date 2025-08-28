9e3060ca1ae75b71c006d842bf7028829d2db634
Kwame Oduro <odurokwameee@gmail.com>
2025-08-28 17:24:22 +0000

    test: add firefox runner for executing tests in firefox browser

---
+++ src/test/java/com/swaglabs/runners/FirefoxRunner.java
@@ -0,0 +1,29 @@
+package com.swaglabs.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+/**
+ * Firefox Test Runner - Forces Firefox browser execution
+ */
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = {"com/swaglabs", "com/swaglabs"},
+        plugin = {
+                "pretty",
+                "html:target/firefox-reports/html-report",
+                "json:target/firefox-reports/cucumber.json",
+                "junit:target/firefox-reports/cucumber.xml"
+        },
+        monochrome = true,
+        tags = "@swag_labs"
+)
+public class FirefoxRunner {
+
+    static {
+        // Force Firefox browser for this runner
+        System.setProperty("browser", "firefox");
+    }
+}
