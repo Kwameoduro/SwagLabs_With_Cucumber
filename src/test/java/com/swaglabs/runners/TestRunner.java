9e17c8ad80152c6ab90be19207342947a9ef3468
Kwame Oduro <odurokwameee@gmail.com>
2025-08-28 17:20:50 +0000

    test: add test runner for executing cucumber scenarios

---
+++ src/test/java/com/swaglabs/runners/TestRunner.java
@@ -0,0 +1,21 @@
+package com.swaglabs.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = {"com/swaglabs", "com/swaglabs"},
+        plugin = {
+                "pretty",
+                "io.cucumber.core.plugin.HtmlFormatter:target/cucumber-reports/cucumber-html-report.html",
+                "json:target/cucumber-reports/cucumber.json"
+        },
+        monochrome = true,
+        tags = "@swag_labs"
+)
+public class TestRunner {
+}
