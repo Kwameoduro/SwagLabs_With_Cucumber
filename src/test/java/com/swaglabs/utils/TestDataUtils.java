f9f6e5dfa4b5e63e38e67f57d4aa23ecdc4b25e0
Kwame Oduro <odurokwameee@gmail.com>
2025-08-29 17:07:26 +0000

    utils: update test data

--- src/test/java/com/swaglabs/utils/TestDataUtils.java
+++ src/test/java/com/swaglabs/utils/TestDataUtils.java
@@ -5,5 +5,4 @@ public class TestDataUtils {
     public static final String CURRENT_USER = "current_user";
     public static final String CURRENT_PRODUCT = "current_product";
     public static final String CHECKOUT_INFO = "checkout_info";
-
 }
