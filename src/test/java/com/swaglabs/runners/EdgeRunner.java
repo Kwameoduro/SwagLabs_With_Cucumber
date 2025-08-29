1bc0deee770ebed7a8142aad3bce600f6c4e7233
Kwame Oduro <odurokwameee@gmail.com>
2025-08-29 12:09:10 +0000

    test: add edge runner for executing tests in firefox browser

---
+++ src/test/java/com/swaglabs/runners/EdgeRunner.java
@@ -0,0 +1,27 @@
+package com.swaglabs.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = {"com/swaglabs", "com/swaglabs"},
+        plugin = {
+                "pretty",
+                "html:target/edge-reports/html-report.tml",
+                "json:target/edge-reports/cucumber.json",
+                "junit:target/edge-reports/cucumber.xml"
+        },
+        monochrome = true,
+        tags = "@swag_labs"
+)
+public class EdgeRunner {
+
+    static {
+        // Force Chrome browser for this runner
+        System.setProperty("browser", "edge");
+    }
+}
